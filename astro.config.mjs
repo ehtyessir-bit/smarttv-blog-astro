60529946fdf33cdf0886b571b6640478533ced8d
ehtyessir-bit <eht.yessir@gmail.com>
2026-06-01 04:26:22 +0100

    init: astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://blog.smarttv.one',
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+  ],
+  markdown: {
+    shikiConfig: { theme: 'github-dark' },
+  },
+});
